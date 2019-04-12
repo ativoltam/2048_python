135f448fbc712eae681a506b44f67715794126d2
samuf <farkas.david05@gmail.com>
2019-04-12 13:51:11 +0200

    index

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
-import alone_2048
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'so amazingly secret' # for session values
+app.config['SECRET_KEY'] = 'so amazingly secret'  # for session values
 
 
 @app.route("/")
 def main():
-	return render_template('index.html')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()

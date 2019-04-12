b7f2b2c705d074348a7e77691b2ae05246137efe
samuf <farkas.david05@gmail.com>
2019-04-12 15:48:27 +0200

    jsonify table added and printed on html

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'so amazingly secret'  # for session values
 
 
-@app.route("/")
-def main():
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run()
+import alone_2048

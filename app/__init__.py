1e2537b32dd9d0a2411ecb57bcb33b4c68e45b32
samuf <farkas.david05@gmail.com>
2019-04-12 13:38:43 +0200

    index.html added

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import alone_2048
 
 app = Flask(__name__)
@@ -7,4 +7,4 @@ app.config['SECRET_KEY'] = 'so amazingly secret' # for session values
 
 @app.route("/")
 def main():
-	return alone_2048.print_board()
+	return render_template('index.html')

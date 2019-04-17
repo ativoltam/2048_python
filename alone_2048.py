7755a35ebc10335d6fe25c838dd597e99ebc8f1a
samuf <farkas.david05@gmail.com>
2019-04-17 17:03:23 +0200

    Half way there

--- alone_2048.py
+++ alone_2048.py
@@ -7,69 +7,49 @@ from app import app
 from game import *
 from flask import request, redirect, url_for, json, render_template
 
-global_list = []
+global_dict = {}
 @app.route("/")
 def main(board=None):
     if board is None:
         b = Game()
-        uid = time.time()
-        local_dict = {uid: b.x}
-        global_list.append(local_dict)
+        uId = time.time()
+        global_dict[uId] = b
         b.add_number()
         return render_template('index.html', table=json.dumps(b.x))
     return render_template('index.html', table=json.dumps(board))
 
 
-@app.route('/play_the_game', methods=['GET', 'POST'])
+@app.route('/play_the_game')
 def play_the_game():
     resp = request.get_json()
-    sessionId = resp['sessionId']
+    uId = resp['uId']
     direction = resp['direction']
-    game = global_list['sessionId']
-    table = game.move(direction)
-    return table
-    direction_forward = request.form.get('w')
-    direction_backward = request.form.get('s')
-    direction_left = request.form.get('a')
-    direction_right = request.form.get('d')
-    if direction_forward is not None:
-        moved = board.process_move(direction_forward)
-        if moved: board.add_number()
-        return redirect(url_for('main'))
-    if direction_backward is not None:
-        moved = board.process_move(direction_backward)
-        if moved: board.add_number()
-        return redirect(url_for('main'))
-    if direction_left is not None:
-        moved = board.process_move(direction_left)
-        if moved: board.add_number()
-        return redirect(url_for('main'))
-    if direction_right is not None:
-        moved = board.process_move(direction_right)
-        if moved: board.add_number()
-        return redirect(url_for('main'))
+    board = global_dict[uId]
+    moved = board.process_move(direction)
+    if moved: board.add_number()
+    return json.dumps(board.x)
 
 
-@app.route('/play_the_game/api/moves/<string:move>')
-def make_move(board, move):
-    if move == "up":
-        moved = board.process_move("w")
-        if moved: board.add_number()
-        return redirect(url_for('main'))
-    if move == "down":
-        moved = board.process_move("s")
-        if moved: board.add_number()
-        return redirect(url_for('main'))
-    if move == "left":
-        moved = board.process_move("a")
-        if moved: board.add_number()
-        return redirect(url_for('main'))
-    if move == "right":
-        moved = board.process_move("d")
-        if moved: board.add_number()
-        return redirect(url_for('main'))
-    else:
-        return "Invalid move!"
+# @app.route('/play_the_game/api/moves/<string:move>')
+# def make_move(board, move):
+#     if move == "up":
+#         moved = board.process_move("w")
+#         if moved: board.add_number()
+#         return redirect(url_for('main'))
+#     if move == "down":
+#         moved = board.process_move("s")
+#         if moved: board.add_number()
+#         return redirect(url_for('main'))
+#     if move == "left":
+#         moved = board.process_move("a")
+#         if moved: board.add_number()
+#         return redirect(url_for('main'))
+#     if move == "right":
+#         moved = board.process_move("d")
+#         if moved: board.add_number()
+#         return redirect(url_for('main'))
+#     else:
+#         return "Invalid move!"
 
 
 @app.route('/play_the_game/api/new_game')
@@ -77,4 +57,4 @@ def new_game():
     board = Game()
     board.new_board()
     board.add_number()
-    return redirect(url_for('main'))
+    return json.dumps(board.x)

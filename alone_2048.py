8aa03d8eb9b10bc9e08859b1d32ee6842e800a89
samuf <farkas.david05@gmail.com>
2019-04-30 13:32:08 +0200

    reworked for dict, not session

--- alone_2048.py
+++ alone_2048.py
@@ -4,6 +4,7 @@ from game import *
 from flask import request, render_template, jsonify, session
 
 
+global_dict = {}
 @app.route("/")
 def main():
     return render_template('index.html')
@@ -14,51 +15,60 @@ def play_the_game():
     resp = request.get_json()
     uId = str(resp['uId'])
     direction = resp['direction']
-    b = pickle.loads(session['uId'])  # Itt a b az maga egy Game object, amit a sessionben tárolt uId alapján hívunk meg
-    board = b.x  # Maga a board (16 db szám) -- A b Game object x paramétere. (mátrix)
-    moved = b.process_move(direction)  # A játék (b) process_move függvényének a visszatérési értéke (boolean)
-    legit = b.next_step_check()  # A játék (b) next_step_check függvényének a visszatérési értéke (boolean)
-    c_score = b.c_score  # A játék (b) c_score (mint current score) változója
+    b = global_dict[uId]
+    # b = pickle.loads(session['dict'][uId])
+    # b = session['dict'][uId]
+    board = b.x
+    moved = b.process_move(direction)
+    legit = b.next_step_check()
+    c_score = b.c_score
     if legit:
         if moved and b.count_zeroes() != 0:
             b.add_number()
             game_data = {"board": board, "c_score": c_score, "uId": uId, "game_over": False}
             game_dict = jsonify(game_data)
-            session['uId'] = pickle.dumps(b)
+            # session['dict'][uId] = pickle.dumps(b)
             return game_dict
         elif moved:
             game_data = {"board": board, "c_score": c_score, "uId": uId, "game_over": False}
             game_dict = jsonify(game_data)
-            session['uId'] = pickle.dumps(b)
+            # session['dict'][uId] = pickle.dumps(b)
             return game_dict
         else:
             game_data = {"board": board, "c_score": c_score, "uId": uId, "game_over": False}
             game_dict = jsonify(game_data)
-            session['uId'] = pickle.dumps(b)
+            # session['dict'][uId] = pickle.dumps(b)
             return game_dict
     game_data = {"board": board, "c_score": c_score, "uId": uId, "game_over": True}
     game_dict = jsonify(game_data)
     return game_dict
 
 
+@app.route('/api/games')
+def games():
+    return str(global_dict)
+
+
 @app.route('/api/new_game')
 def new_game():
-    b = Game()  # Egy új Game object.
+    b = Game()
     uId = str(time.time())
-    b.add_number()  # A játék (b) add_number metódusa meghívása, amivel elhelyez 2 random tile-t a board-on.
-    board = b.x  # Maga a board (16 db szám) -- A b Game object x paramétere. (mátrix)
-    c_score = b.c_score  # A játék (b) c_score (mint current score) változója
+    b.add_number()
+    board = b.x
+    c_score = b.c_score
     game_data = {"board": board, "c_score": c_score, "uId": uId}
     game_dict = jsonify(game_data)
-    session['uId'] = pickle.dumps(b)
+    # session['dict'][uId] = pickle.dumps(b)
+    # b = global_dict[uId]
+    global_dict[uId] = b
     return game_dict
 
 
 @app.route('/save_user_highscore', methods=['POST', 'GET']) #curl -H 'Content-Type: application/json' -X GET 127.0.0.1:5000/save_user_highscore -d '{"u_name": "test_1", "c_score": 1000}'
 def save_user_highscore():
     resp = request.get_json()
-    u_name = resp['u_name']  # User name rövidítése, front-end-en user adja meg, ezzel mentjük el az adatbázisba
-    c_score = resp['c_score']  # Current score, front-end-en user adja meg, ezzel mentjük el az adatbázisba
+    u_name = resp['u_name']
+    c_score = resp['c_score']
     db.save_to_db(u_name, c_score)
     msg = "Saved!"
     return msg
